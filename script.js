d78148b56243c064bbf9e4c0e6f8da642e6b45ef
NightDHero <heroboyxds@gmail.com>
2024-08-31 23:27:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openPopup(popupId) {
+    document.getElementById(popupId).style.display = "block";
+}
+
+function closePopup(popupId) {
+    document.getElementById(popupId).style.display = "none";
+}
+
+// Search bar for searching usages
